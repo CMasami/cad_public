4c37cf0471f80ef5a8685cbd252da6e2b9a1c745
Chikahiro Masami <zio_Luigi@hotmail.com>
2024-06-22 22:10:35 +0900

    hellocs first step

---
+++ csharp/hellocs/Class1.cs
@@ -0,0 +1,24 @@
+﻿using Itjp.Ijcad.ApplicationServices;
+using Itjp.Ijcad.DatabaseServices;
+using Itjp.Ijcad.EditorInput;
+using Itjp.Ijcad.Runtime;
+
+namespace hellocs
+{
+    public class Class1
+    {
+        [CommandMethod("CSHELLO")]
+        public void cmdCsHello()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            ed.WriteMessage("\nHello, World.");
+        }
+
+        [CommandMethod("CSHELLOMSG")]
+        public void cmdCsHelloMsg()
+        {
+            Application.ShowAlertDialog("Hello, World");
+        }
+    }
+}
