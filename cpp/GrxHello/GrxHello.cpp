cb01d04932cf76ab49d1111136f158bd0f5092da
Chikahiro Masami <zio_Luigi@hotmail.com>
2024-06-22 22:33:41 +0900

    Cpp version

---
+++ cpp/GrxHello/GrxHello.cpp
@@ -0,0 +1,66 @@
+﻿// GrxHello.cpp : DLL の初期化ルーチンを定義します。
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "GrxHello.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+void cmdHello()
+{
+    acutPrintf(_T("\nHello World."));
+}
+
+void cmdHelloMsg()
+{
+    acedAlert(_T("Hello World."));
+}
+
+void initApp()
+{
+    acedRegCmds->addCommand(_T("ASDK_SAMPLES_HELLOWORLD"), _T("ASDK_HELLOWORLD"), _T("HELLOWORLD"), ACRX_CMD_MODAL, cmdHello);
+    acedRegCmds->addCommand(_T("ASDK_SAMPLES_HELLOWORLD"), _T("ASDK_HELLODLG"), _T("HELLOMSG"), ACRX_CMD_MODAL, cmdHelloMsg);
+}
+
+
+void unloadApp()
+{
+    acedRegCmds->removeGroup(_T("ASDK_SAMPLES_HELLOWORLD"));
+}
+
+
+extern "C" AcRx::AppRetCode
+acrxEntryPoint(AcRx::AppMsgCode msg, void* appId)
+{
+    switch (msg) {
+    case AcRx::kInitAppMsg:
+        acrxDynamicLinker->unlockApplication(appId);
+        acrxDynamicLinker->registerAppMDIAware(appId);
+        initApp();
+        break;
+    case AcRx::kUnloadAppMsg:
+        unloadApp();
+    }
+    return AcRx::kRetOK;
+}
+
+
+// CGrxHelloApp
+
+BEGIN_MESSAGE_MAP(CGrxHelloApp, CWinApp)
+END_MESSAGE_MAP()
+
+CGrxHelloApp::CGrxHelloApp()
+{
+}
+
+CGrxHelloApp theApp;
+
+BOOL CGrxHelloApp::InitInstance()
+{
+	CWinApp::InitInstance();
+	return TRUE;
+}
---
+++ cpp/GrxHello/GrxHello.h
@@ -0,0 +1,27 @@
+﻿// GrxHello.h : GrxHello DLL のメイン ヘッダー ファイル
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CGrxHelloApp
+// このクラスの実装に関しては GrxHello.cpp をご覧ください
+//
+
+class CGrxHelloApp : public CWinApp
+{
+public:
+	CGrxHelloApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
