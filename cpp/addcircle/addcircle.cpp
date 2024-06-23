2c088e23ef21ef3da4665b509358bcd7abaad210
Chikahiro Masami <zio_Luigi@hotmail.com>
2024-06-23 12:34:11 +0900

    finish command

--- cpp/addcircle/addcircle.cpp
+++ cpp/addcircle/addcircle.cpp
@@ -3,13 +3,85 @@
 
 #include "pch.h"
 
-
 void cmdAddCircle()
 {
-
+	ads_point ptres;
+	int rc = ads_getpoint(NULL, L"\n中心を指定: ", ptres);
+	if (rc != RTNORM)
+		return;
+	resbuf rb;
+	ads_getvar(L"CIRCLERAD", &rb);
+	double radius = rb.resval.rreal;
+	ads_initget(RSG_NONEG | RSG_NOZERO, NULL);
+	AcString prompt;
+	prompt.format(L"\n半径を指示<%f>: ", radius);
+	rc = ads_getdist(ptres, prompt, &radius);
+	if (rc == RTNONE)
+	{
+		radius = rb.resval.rreal;
+		rc = RTNORM;
+	}
+	if (rc != RTNORM)
+		return;
+	AcTransactionManager* tr = acTransactionManagerPtr();
+	try
+	{
+		Acad::ErrorStatus es;
+		tr->startTransaction();
+		AcDbDatabase* db = curDoc()->database();
+		AcDbBlockTableRecord* space;
+		es = tr->getObject((AcDbObject*&)space, db->currentSpaceId(), AcDb::kForWrite);
+		if (es)	throw es;
+		AcDbCircle* circle = new AcDbCircle();
+		circle->setDatabaseDefaults(db);
+		circle->setCenter(asPnt3d(ptres));
+		circle->setRadius(radius);
+		es = space->appendAcDbEntity(circle);
+		if (es)	throw es;
+		es = tr->addNewlyCreatedDBRObject(circle, true);
+		if (es)	throw es;
+		tr->endTransaction();
+		rb.resval.rreal = radius;
+		ads_setvar(L"CIRCLERAD", &rb);
+	}
+	catch (Acad::ErrorStatus es)
+	{
+		ads_printf(acadErrorStatusText(es));
+		tr->abortTransaction();
+	}
 }
 
 void cmdAddCircle2()
 {
-
+	ads_point ptres;
+	int rc = ads_getpoint(NULL, L"\n中心を指定: ", ptres);
+	if (rc != RTNORM)
+		return;
+	resbuf rb;
+	ads_getvar(L"CIRCLERAD", &rb);
+	double radius = rb.resval.rreal;
+	ads_initget(RSG_NONEG | RSG_NOZERO, NULL);
+	AcString prompt;
+	prompt.format(L"\n半径を指示<%f>: ", radius);
+	rc = ads_getdist(ptres, prompt, &radius);
+	if (rc == RTNONE)
+	{
+		radius = rb.resval.rreal;
+		rc = RTNORM;
+	}
+	if (rc != RTNORM)
+		return;
+	resbuf* elist = ads_buildlist(
+		RTDXF0, L"CIRCLE",
+		10, ptres,
+		40, radius,
+		RTNONE
+	);
+	rc = ads_entmake(elist);
+	ads_relrb(elist);
+	if (rc == RTNORM)
+	{
+		rb.resval.rreal = radius;
+		ads_setvar(L"CIRCLERAD", &rb);
+	}
 }
